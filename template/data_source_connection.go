a1d1aa44471b44661de518ff31034f874d3368eb
Sam Myers <sam@sam-myers.com>
2019-09-01 14:33:55 -0500

    Create skeleton of data source connection
    Signed-off-by: Sam Myers <sam@sam-myers.com>

---
+++ template/data_source_connection.go
@@ -0,0 +1,41 @@
+package template
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func dataSourceLinuxSSHConnection() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"host": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The address of the resource to connect to",
+			},
+			"user": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "root",
+				Description: "The user that we should use for the connection",
+			},
+			"port": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     22,
+				Description: "The port to connect to",
+			},
+			"password": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Description:   "The password we should use for the connection",
+				ConflictsWith: []string{"private_key"},
+			},
+			"private_key": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Description:   "The contents of an SSH key to use for the connection",
+				ConflictsWith: []string{"password"},
+			},
+		},
+	}
+}
--- template/provider.go
+++ template/provider.go
@@ -8,19 +8,7 @@ import (
 func Provider() terraform.ResourceProvider {
 	return &schema.Provider{
 		DataSourcesMap: map[string]*schema.Resource{
-			"template_file":             dataSourceFile(),
-			"template_cloudinit_config": dataSourceCloudinitConfig(),
-		},
-		ResourcesMap: map[string]*schema.Resource{
-			"template_file": schema.DataSourceResourceShim(
-				"template_file",
-				dataSourceFile(),
-			),
-			"template_cloudinit_config": schema.DataSourceResourceShim(
-				"template_cloudinit_config",
-				dataSourceCloudinitConfig(),
-			),
-			"template_dir": resourceDir(),
+			"linux_ssh_connection": dataSourceLinuxSSHConnection(),
 		},
 	}
 }
